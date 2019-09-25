bec044b88ef1f036d1853816d6fdb877b8d55a55
p5-vbnekit <vbnekit@p5y.su>
2019-09-25 21:44:48 +0300

    Update download-tar.sh

--- tools/download-tar.sh
+++ tools/download-tar.sh
@@ -11,8 +11,7 @@ routine1() {
 }
 
 routine0() {
-  IFS="\n"
-  for m_md5_line in $(wget -O - "${m_base_url}/${m_base_name}.tar.md5.txt"); do
+  while read m_md5_line; do
     m_md5_hash=$(echo -n "${m_md5_line}" | sed -rne 's/^([0-9a-f]{32} [ \*])[^\/]+\.[0-9]{2}$/\1/p') || return $?
     test -n "${m_md5_hash}" || return $?
     m_md5_name=$(echo -n "${m_md5_line}" | tail -c +35) || return $?
@@ -27,7 +26,7 @@ routine0() {
 routine() {
   m_result=0
   m_temp_directory=$(mktemp --directory "download.XXXXXX") || return $?
-  (cd "${m_temp_directory}" && routine0) || m_result=$?
+  (cd "${m_temp_directory}" && wget -O - "${m_base_url}/${m_base_name}.tar.md5.txt") | routine0) || m_result=$?
   rm -rf "${m_temp_directory}" || true
   return ${m_result}
 }
