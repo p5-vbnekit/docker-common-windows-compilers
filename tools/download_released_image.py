640ac2510481a884823164fb238f774b329412d6
Nikita Pushchin <vbnekit@p5y.su>
2019-09-26 21:35:28 +0300

    ...

--- tools/download_released_image.py
+++ tools/download_released_image.py
@@ -14,7 +14,7 @@ yaml-style options string:
   buffer_size: integer, None or False for default; default is 128 * 1024 * 1024
 """
 
-import os, io, re, sys, yaml, ctypes, hashlib, github, urllib.request
+import os, io, re, sys, yaml, time, ctypes, hashlib, github, traceback, urllib.request
 
 if "__main__" == __name__: from __main__ import __file__ as main_path
 
@@ -142,6 +142,7 @@ def routine():
     with urllib.request.urlopen(m_asset.browser_download_url) as m_response: return tuple([Record(m_line.decode("utf-8")) for m_line in m_response.readlines()])
 
   m_buffer = ctypes.create_string_buffer(m_options.buffer_size)
+  m_buffer_length = len(m_buffer)
 
   def make_output():
     if m_options.output:
@@ -152,9 +153,10 @@ def routine():
           self.__stream = open(m_options.output, "wb")
         @staticmethod
         def is_regular_file(): return True
+        def seek(self, offset): return self.__stream.seek(offset, io.SEEK_SET)
         def write(self, data): return self.__stream.write(data)
         def flush(self): return self.__stream.flush()
-        def close(self): return self.__close()
+        def close(self): return self.__stream.close()
         def remove(self):
           m_path = self.__path
           self.__path = None
@@ -182,29 +184,86 @@ def routine():
   try:
     def process_asset(md5_record):
       m_asset = m_assets[md5_record.name]
-      m_log("{}: processing asset \"{}\", size = {}, expected md5 = \"{}\", download url = \"{}\"".format(main_path, md5_record.name, m_asset.size, md5_record.digest, m_asset.browser_download_url), file = sys.stderr)
       if not (0 < m_asset.size): return
 
-      m_md5 = hashlib.md5()
-      with urllib.request.urlopen(m_asset.browser_download_url) as m_response:
-        m_total = [0]
+      def make_md5(): return hashlib.md5()
+
+      def make_context():
+        class Result(object):
+          def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.offset = 0
+            self.md5 = make_md5()
+        return Result()
+
+      m_context = make_context()
+
+      def make_input():
         def routine():
-          m_size = m_response.readinto(m_buffer)
-          if not (0 < m_size): return False
-          m_md5.update(m_buffer[:m_size])
-          m_total[0] += m_size
-          m_output.write(m_buffer[:m_size])
-          m_output.flush()
-          m_log("{}: {} bytes of asset \"{}\" was processed, download url = \"{}\"".format(main_path, md5_record.name, m_total[0], m_asset.browser_download_url), file = sys.stderr)
-          return True
+          m_headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36"}
+          if (0 < m_context.offset): m_headers["Range"] = "bytes={}-".format(m_context.offset)
+          return urllib.request.urlopen(urllib.request.Request(url = m_asset.browser_download_url, headers = m_headers))
+        m_try_number = 0
+        while True:
+          m_try_number += 1
+          m_log("{}: trying to open url, try #{}, asset = \"{}\", url = \"{}\"".format(main_path, m_try_number, md5_record.name, m_asset.browser_download_url), file = sys.stderr)
+          try: return routine()
+          except OSError:
+            traceback.print_exc(file = sys.stderr)
+            if not (3 > m_try_number): raise
+          except: raise
+          m_log("{}: waiting 5 seconds".format(main_path), file = sys.stderr)
+          time.sleep(+5.0e+0)
 
-        if not routine(): raise RuntimeError("unexpected EOF")
-        while routine(): pass
+      def routine(try_number):
+        with make_input() as m_input:
+          def routine():
+            try: m_size = m_input.readinto(m_buffer)
+            except (OSError):
+              if not (3 > try_number): raise
+              traceback.print_exc(file = sys.stderr)
+              return False
+            try:
+              if not (0 < m_size): raise RuntimeError("unexpected EOF")
+              m_expected = min(m_asset.size - m_context.offset, m_buffer_length)
+              if not (m_size <= m_expected): raise OverflowError("unexpected data")
+            except:
+              if not (3 > try_number): raise
+              traceback.print_exc(file = sys.stderr)
+              return False
+            if m_size != m_output.write(m_buffer[:m_size]): raise RuntimeError("failed to write")
+            m_output.flush()
+            m_context.md5.update(m_buffer[:m_size])
+            m_context.offset += m_size
+            return True
+          while m_context.offset < m_asset.size:
+            if not routine(): return False
 
-        m_md5_digest = m_md5.hexdigest()
-        m_log("{}: asset \"{}\" was processed, size = {}, calculated md5 = \"{}\", download url = \"{}\"".format(main_path, md5_record.name, m_total[0], m_md5_digest, m_asset.browser_download_url), file = sys.stderr)
-        if m_asset.size != m_total[0]: raise RuntimeError("size mismatch")
-        if md5_record.digest != m_md5_digest: raise RuntimeError("md5 mismatch")
+        try:
+          m_md5_digest = m_context.md5.hexdigest()
+          m_log("{}: asset \"{}\" was processed, size = {}, calculated md5 = \"{}\", download url = \"{}\"".format(main_path, md5_record.name, m_context.offset, m_md5_digest, m_asset.browser_download_url), file = sys.stderr)
+          if m_asset.size != m_context.offset: raise RuntimeError("size mismatch")
+          if md5_record.digest != m_md5_digest: raise RuntimeError("md5 mismatch")
+        except:
+          if not m_output.is_regular_file(): raise
+          if not (3 > try_number): raise
+          try:
+            m_output.seek(0)
+            m_output.truncate(0)
+            m_context.md5 = make_md5()
+            traceback.print_exc(file = sys.stderr)
+            return False
+          except: traceback.print_exc(file = sys.stderr)
+          raise
+        return True
+
+      m_try_number = 0
+      while True:
+        m_try_number += 1
+        m_log("{}: processing asset \"{}\", try #{}, expected size = {}, expected md5 = \"{}\", download url = \"{}\"".format(main_path, md5_record.name, m_try_number, m_asset.size, md5_record.digest, m_asset.browser_download_url), file = sys.stderr)
+        if routine(m_try_number): break
+        m_log("{}: waiting 5 seconds".format(main_path), file = sys.stderr)
+        time.sleep(+5.0e+0)
 
     for m_md5_record in make_md5(): process_asset(m_md5_record)
     m_output.flush()
