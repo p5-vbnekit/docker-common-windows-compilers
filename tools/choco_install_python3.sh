c6c76f1b89bb7ea5b6fa0e4839098a32c96e03aa
Nikita Pushchin <vbnekit@p5y.su>
2019-09-29 02:59:40 +0300

    ...

--- tools/choco_install_python3.sh
+++ tools/choco_install_python3.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -e
 
 m_normal_path="$1"
-export PS4="executing command: "; set -xo pipefail
+export PS4="executing command: "; set -x
 
 test -n "${m_normal_path}"
 mkdir -p "${m_normal_path}"
-m_ugly_path=$(cd "${m_normal_path}" && cmd.exe /c 'echo %cd%' | tail -n 1)
+m_ugly_path=`((((cd "${m_normal_path}" && "cmd.exe" "/c" 'echo %cd%'; echo "$?" >&3) | tail -n 1 >&4) 3>&1) | (read xs; exit $xs)) 4>&1`
 test -n "${m_ugly_path}"
 choco install "python3" --confirm --params "/InstallDir:\"${m_ugly_path}\""
