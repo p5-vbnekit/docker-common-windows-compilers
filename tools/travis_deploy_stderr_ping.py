20929ca2757fc5df56d394e94ab94133b4de7566
Nikita Pushchin <vbnekit@p5y.su>
2019-09-29 03:14:36 +0300

    ...

--- tools/travis_deploy_stderr_ping.py
+++ tools/travis_deploy_stderr_ping.py
@@ -7,5 +7,5 @@ if "__main__" == __name__:
   from __main__ import __file__ as m_main_path
   m_begin = time.monotonic()
   while True:
-    time.sleep(+3.0e+2)
+    time.sleep(+3.0e+0)
     print("{}: this message had to be sent, otherwise travis-ci will stop the the deployment of large files by 10 minutes timeout, total time elapsed: {} seconds...".format(m_main_path, time.monotonic() - m_begin), file = sys.stderr)
