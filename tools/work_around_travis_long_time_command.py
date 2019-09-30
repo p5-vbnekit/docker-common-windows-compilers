b0cd7b45f1ca94b511d63aebdcb087ab27f449e6
p5-vbnekit <vbnekit@p5y.su>
2019-10-01 01:02:54 +0300

    Update work_around_travis_long_time_command.py

--- tools/work_around_travis_long_time_command.py
+++ tools/work_around_travis_long_time_command.py
@@ -37,7 +37,7 @@ if "__main__" == __name__:
         m_context.barrier_time_point = m_current_time_point + m_context.wait_duration
         print(m_message_template.format(m_main_path, (m_current_time_point - m_context.last_time_point) / +6.0e+1, (m_current_time_point - m_context.initial_time_point) / +6.0e+1), file = sys.stderr)
         sys.stderr.flush()
-      print("{}: exiting".format(m_main_path), file = sys.stderr)
+      print("{}: total time elapsed {} minutes, exiting...".format(m_main_path, (time.monotonic() - m_context.initial_time_point) / +6.0e+1), file = sys.stderr)
       sys.stderr.flush()
 
   m_command = sys.argv[1:]
