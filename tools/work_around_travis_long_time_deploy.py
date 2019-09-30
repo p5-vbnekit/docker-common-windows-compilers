f71c7b12842e06ffe65d7345ce9035b1d9e42205
p5-vbnekit <vbnekit@p5y.su>
2019-10-01 01:05:36 +0300

    Update work_around_travis_long_time_deploy.py

--- tools/work_around_travis_long_time_deploy.py
+++ tools/work_around_travis_long_time_deploy.py
@@ -8,7 +8,7 @@ if "__main__" == __name__:
   m_begin = time.monotonic()
   while True:
     time.sleep(+2.0e+2)
-    print("{}: this message had to be sent, otherwise travis-ci will stop the the deployment of large files by 10 minutes timeout, total time elapsed: {} minutes...".format(m_main_path, (time.monotonic() - m_begin) / +6.0e+1), file = sys.stderr)
+    print("{}: this message had to be sent, otherwise travis-ci will stop the the deployment of large files by 10 minutes timeout, time elapsed: {} minutes...".format(m_main_path, (time.monotonic() - m_begin) / +6.0e+1), file = sys.stderr)
     sys.stderr.flush()
-  print("{}: total time elapsed: {} minutes, exiting...".format(m_main_path, (time.monotonic() - m_begin) / +6.0e+1), file = sys.stderr)
+  print("{}: time elapsed: {} minutes, exiting...".format(m_main_path, (time.monotonic() - m_begin) / +6.0e+1), file = sys.stderr)
   sys.stderr.flush()
