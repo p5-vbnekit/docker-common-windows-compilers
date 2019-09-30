bad0def7e4dd20cdc7afd1609e8bfbba1351ee15
Nikita Pushchin <vbnekit@p5y.su>
2019-09-30 22:44:26 +0300

    ...

--- tools/work_around_travis_long_time_deploy.py
+++ tools/work_around_travis_long_time_deploy.py
@@ -7,6 +7,6 @@ if "__main__" == __name__:
   from __main__ import __file__ as m_main_path
   m_begin = time.monotonic()
   while True:
-    time.sleep(+1.2e+2)
+    time.sleep(+2.0e+2)
     print("{}: this message had to be sent, otherwise travis-ci will stop the the deployment of large files by 10 minutes timeout, total time elapsed: {} minutes...".format(m_main_path, (time.monotonic() - m_begin) / +6.0e+1), file = sys.stderr)
     sys.stderr.flush()
